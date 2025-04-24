57e0aa0f2e041fff5e620a712cb91da1f8b6f6ad
GuilhermeSzFernandes <guilhermenovo3001@gmail.com>
2025-04-24 08:08:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const puppeteer = require('puppeteer');
 app.get('/', async (req, res) => {
   const browser = await puppeteer.launch({
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
-    headless: true,
+    headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
   await page.goto('https://example.com');
+  await page.waitForSelector('canvas[aria-label="Scan me!"]', { timeout: 0 });
+  console.log('QR Code carregado!');
   const title = await page.title();
   await browser.close();
   res.send(`Título da página: ${title}`);
